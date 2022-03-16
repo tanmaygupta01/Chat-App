c7e3864d3431df6813712b4ffc7efea7159e7bf2
gupta.tanmay01@gmail.com <gupta.tanmay01@gmail.com>
2022-03-16 22:13:47 +0530

    Chat App

---
+++ src/index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
+
+const app = express()
+const server = http.createServer(app);
+const io = socketio(server);
+const Filter = require('bad-words');
+const { generateMessage, generateLocationMessage } = require('./utils/messages');
+const { getUser, addUser, removeUser, getUsersInRoom } = require('./utils/users');
+
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket Connection');
+
+    socket.on('join', (options, callback) => {
+
+        const { error, user } = addUser({ id: socket.id, ...options });
+
+        if (error) {
+            return callback(error);
+        }
+
+        socket.join(user.room)
+
+        socket.emit('message', generateMessage('Welcome!'));
+        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`));
+
+        callback()
+    })
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id)
+        const filter = new Filter();
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!');
+        }
+
+        io.to(user.room).emit('message', generateMessage(message));
+        callback('Delivered!');
+    })
+
+    socket.on('sendLocation', (coords, callback) => {
+        const user = getUser(socket.id)
+        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `http://google.com/maps?q=${coords.latitude},${coords.longitude}`));
+        callback();
+    })
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('message', generateMessage(`${user.username} has left!`));
+        }
+    });
+})
+
+server.listen(port, () => {
+    console.log(`Server is up on ${port}! `)
+});
